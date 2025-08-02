459f903ecef3901b1ec086ff999d3f7771c8f89f
Jkennedy2004 <jg374997@gmail.com>
2025-08-02 04:02:31 -0500

    prueba8

--- app/index.js
+++ app/index.js
@@ -1,3 +1,32 @@
+// const express = require('express');
+// const sqlite3 = require('sqlite3').verbose();
+// const app = express();
+// const port = 3000;
+
+// let db = new sqlite3.Database(':memory:');
+
+// db.serialize(() => {
+//   db.run("CREATE TABLE users (id INT, name TEXT)");
+//   db.run("INSERT INTO users VALUES (1, 'Alice')");
+// });
+
+// app.get('/user', (req, res) => {
+//   const name = req.query.name;
+//   const query = `SELECT * FROM users WHERE name = '${name}'`; // vulnerable
+//   db.all(query, [], (err, rows) => {
+//     if (err) {
+//       res.status(500).send('Error');
+//     } else {
+//       res.json(rows);
+//     }
+//   });
+// });
+
+// app.listen(port, () => {
+//   console.log(`App listening at http://localhost:${port}`);
+// });
+
+
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
@@ -12,8 +41,9 @@ db.serialize(() => {
 
 app.get('/user', (req, res) => {
   const name = req.query.name;
-  const query = `SELECT * FROM users WHERE name = '${name}'`; // vulnerable
-  db.all(query, [], (err, rows) => {
+  const query = `SELECT * FROM users WHERE name = ?`; // consulta parametrizada segura
+
+  db.all(query, [name], (err, rows) => {
     if (err) {
       res.status(500).send('Error');
     } else {
