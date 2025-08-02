f92fc163db7f4531afd5f5763db2a860c762e2df
Jkennedy2004 <jg374997@gmail.com>
2025-08-02 03:41:58 -0500

    prueba2

--- iac/main.tf
+++ iac/main.tf
@@ -1,16 +1,20 @@
-resource "aws_security_group" "bad_example" {
-  name        = "bad_sg"
-  description = "Allow all traffic"
+resource "aws_security_group" "good_example" {
+  name        = "secure_sg"
+  description = "Security group with limited access"
+
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["203.0.113.0/24"]  # Solo acceso desde IP segura
+    description = "Allow SSH from trusted IP"
   }
+
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTPS outbound"
   }
 }
